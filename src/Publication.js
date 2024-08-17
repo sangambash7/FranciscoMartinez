9b9d73287fdad9e391707a746bebfc1b2560172a
sangambash7 <san.gambashidze@gmail.com>
2024-08-17 18:04:30 +0300

    minor

--- src/Publication.js
+++ src/Publication.js
@@ -2,16 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Publication(props) {
-  const handleClick = () => {
-    window.location.href = props.publicationObj.hyperlink;
-  };
-
   return (
-    <li
-      className="publication col-12 col-md-6 col-xxl-3 mb-5"
-      onClick={handleClick}
-      style={{ cursor: "pointer" }}
-    >
+    <li className="publication col-12 col-md-6 col-xxl-3 mb-5">
       <img
         src={`media/publications-imgs/${props.publicationObj.image}`}
         alt={props.publicationObj.namePublication}
@@ -19,7 +11,7 @@ export default function Publication(props) {
       <div>
         <p>{props.publicationObj.namePublication}</p>
         <span>
-          <Link to={`./${props.publicationObj.hyperlink}`}>Read More</Link>
+          <Link to={`/${props.publicationObj.hyperlink}`}>Read More</Link>
         </span>
       </div>
     </li>
