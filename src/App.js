d55098340e79c4659b31c790ffbce3c0f0c657b1
sangambash7 <san.gambashidze@gmail.com>
2024-08-17 13:55:43 +0300

    Hasrouter changed to browserouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import Intro from "./Intro";
 import Introduction from "./Introduction";
